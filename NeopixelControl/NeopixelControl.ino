fc9e55a9f674e6a21dfc317633760009ccf91eb1
tomas1978 <tomas.nilsson78@gmail.com>
2021-03-08 13:58:06 +0100

    Initializing Neopixel
    Added code to create Neopixel object and code to set all pixels to one colour

--- NeopixelControl/NeopixelControl.ino
+++ NeopixelControl/NeopixelControl.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+#include<Adafruit_NeoPixel.h>
 
+#define PIN    2
+#define N_LEDS  24
+
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  ring.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  ring.fill(0,0,0);
+  for(int i=0;i<24;i++) {
+  ring.setPixelColor(i, ring.Color(0,118,189));
+    ring.show();
+    delay(42);
+  }
 }
